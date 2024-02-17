9c71824e7d691d105487160f84bad1241de9d889
Cristhian Arce <cristhianarce30@gmail.com>
2024-02-17 10:38:03 -0500

    Initialize Project

---
+++ src/main/kotlin/com/ccs/ct1signalclassifier/Ct1SignalClassifierApplication.kt
@@ -0,0 +1,11 @@
+package com.ccs.ct1signalclassifier
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ct1SignalClassifierApplication
+
+fun main(args: Array<String>) {
+	runApplication<Ct1SignalClassifierApplication>(*args)
+}
---
+++ src/test/kotlin/com/ccs/ct1signalclassifier/Ct1SignalClassifierApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ccs.ct1signalclassifier
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ct1SignalClassifierApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
